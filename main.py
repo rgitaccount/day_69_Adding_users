dec4b2d7a7418558f6f70ec61c302165ab46eb73
rgitaccount <nbaidoletov@gmail.com>
2021-12-05 19:12:04 +0600

    Add methods "post" and "get" to 'edit_post'

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ def add_new_post():
     return render_template("make-post.html", form=form, current_user=current_user)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
